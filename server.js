4a6832b93c848e335674cbab65f86cfd764c6324
William Henrique Lima Da Silva <william.lima@sp01.local>
2020-04-29 21:20:18 -0300

    Ajuste Json

--- server.js
+++ server.js
@@ -91,30 +91,29 @@ server.post('/auth/login', (req, res) => {
   res.status(200).json({access_token})
 })
 
-server.use(/^(?!\/auth).*$/,  (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Error in authorization format'
-    res.status(status).json({status, message})
-    return
-  }
-  try {
-    let verifyTokenResult;
-     verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+// server.use(/^(?!\/auth).*$/,  (req, res, next) => {
+//   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+//     const status = 401
+//     const message = 'Error in authorization format'
+//     res.status(status).json({status, message})
+//     return
+//   }
+//   try {
+//     let verifyTokenResult;
+//      verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
 
-     if (verifyTokenResult instanceof Error) {
-       const status = 401
-       const message = 'Access token not provided'
-       res.status(status).json({status, message})
-       return
-     }
-     next()
-  } catch (err) {
-    const status = 401
-    const message = 'Error access_token is revoked'
-    res.status(status).json({status, message})
-  }
-})
+//      if (verifyTokenResult instanceof Error) {
+//        const status = 401
+//        res.status(status).json({status, message})
+//        return
+//      }
+//      next()
+//   } catch (err) {
+//     const status = 401
+//     const message = 'Error access_token is revoked'
+//     res.status(status).json({status, message})
+//   }
+// })
 
 server.use(router)
 
